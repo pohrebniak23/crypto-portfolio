9feebb7bd9e6f2f8923ca4d76cb700c5d65bdc07
Oleksandr <pogrebnyak19999@gmail.com>
2022-11-30 12:43:16 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -13,8 +13,7 @@ declare module "@mui/material/styles/createPalette" {
   }
 }
 
-// eslint-disable-next-line
-const app = initializeApp({
+initializeApp({
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_DATABASE_DOMAIN,
